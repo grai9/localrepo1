90bce76a0dcd46aff65e0058ca9c25cc64f7702a
Rai <gagan.rai@dxc.com>
2020-08-12 10:43:07 +0530

    String

---
+++ Test.java
@@ -0,0 +1,10 @@
+class Test
+{
+   public static void main(String[] args)
+   {
+     String s1 = "Hello Web World";
+     String s2 = new String("Welcome To Server");
+     System.out.println(s1);
+     System.out.println(s2);
+   }
+}
